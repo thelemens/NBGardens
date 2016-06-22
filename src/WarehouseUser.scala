922e0c568aa19ebd8673f0b09ebdb1032466fca8
thelemens <mark.a.kelly2@gmail.com>
2016-06-22 23:52:18 +0100

    Nearly finished UI, made changes to accesors in OrderList.

--- src/WarehouseUser.scala
+++ src/WarehouseUser.scala
@@ -6,18 +6,23 @@ object WarehouseUser {
   //view list of orders, view information of order
   def main(args: Array[String]) {
 
-    doUI()
+    doUI(genOrderList())
   }
 
-  def printList(): Unit ={
+  def genOrderList(): OrderList = {
+    new OrderList()
+  }
+
+  def printList(list: OrderList): Unit ={
     //print list of orders  CHANGE THIS TO CONTROL ITEMS?
-    val list = new OrderList
+    var i: Int = 0
     for (item <- list.orders) {
-      println(item)
+      i += 1
+      println(i + ". " + item)
     }
   }
 
-  def doUI(): Unit = {
+  def doUI(list: OrderList): Unit = {
 
     println("\n\n----------------------------\n")
 
@@ -26,12 +31,27 @@ object WarehouseUser {
       println("\nEnter a command:\n1. View order list\n2. Quit\n")
       val i = scala.io.StdIn.readInt()
       i match {
-        case 1 => printList()
+        case 1 => printList(list); doOrderUI(list)
         case 2 => /* println("Existing"); */ run = false
         case whoops => println("Unexpected choice, try again")
       }
     } while (run)
 
     println("Exiting...")
+  }
+
+  def doOrderUI(list: OrderList): Unit = {
+
+    println("-----------\n\nChoose the number of an order, or [0] to exit")
+    var run: Boolean = true
+
+    do {
+
+      val c = scala.io.StdIn.readInt()
+      list.getOrderAtIndex(c)
+
+
+      run = false
+    } while(run)
   }
 }
